8698118eebd202ce4561570c1016dd2d229a6687
Jan Dubina <jan@azavista.com>
2018-01-25 11:50:49 +0100

    Load functions from different files.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,24 @@
-const functions = require('firebase-functions');
+/** EXPORT ALL FUNCTIONS
+ *
+ *   Loads all `.func.js` files in `functions` directory
+ *   Exports a cloud function matching the file name and its path
+ *
+ *   Based on this thread:
+ *      https://github.com/firebase/functions-samples/issues/170
+ *   And this article:
+ *      https://codeburst.io/organizing-your-firebase-cloud-functions-67dc17b3b0da
+ */
 
-// const admin = require('firebase-admin');
-// admin.initializeApp(functions.config().firebase);
+const glob = require('glob');
+const camelCase = require('camelcase');
+
+let files = glob.sync('./**/*.func.js', {cwd: __dirname, ignore: './node_modules/**'});
+
+for (let file of files) {
+    // Strip off '.func.js' from the end of filename
+    let functionName = camelCase(file.slice(0, -8).split('/').join('_'));
+
+    if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
+        exports[functionName] = require(file);
+    }
+}
