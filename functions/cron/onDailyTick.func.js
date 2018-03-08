e5200a4a6f5443756177329977fc71fa9facc550
Jan Dubina <jan@azavista.com>
2018-03-08 16:29:33 +0100

    Add gcloud app that generates cron-like sub/pub messages and a handler that deletes all messages when triggered.

---
+++ functions/cron/onDailyTick.func.js
@@ -0,0 +1,34 @@
+/**
+ *  Gets triggered by pub/sub message every day at 9am
+ *
+ *  Deletes all chat messages.
+ */
+
+const functions = require('firebase-functions');
+
+// admin SDK can be only initialized once, wrap in try-catch
+const admin = require('firebase-admin');
+try {
+    admin.initializeApp(functions.config().firebase);
+} catch (e) {}
+
+
+module.exports = functions.pubsub
+    .topic('daily-tick')
+    .onPublish(event => {
+        return deleteAllMessages();
+    });
+
+const deleteAllMessages = () => {
+    const db = admin.database();
+    const updates = {
+        '/messages': null,
+        '/chats': null
+    };
+
+    console.log('Cron daily at 9am - deleting all messages.');
+
+    // using multipath update to delete nodes instead of remove
+    // as the operation is atomic this way
+    return db.ref().update(updates);
+};
